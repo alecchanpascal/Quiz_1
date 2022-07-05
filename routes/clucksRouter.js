918d2cfe0f7a9e6d5642c8094e2c5cb046e8edc3
Alec Chan <alec.pascal@gmail.com>
2022-07-05 23:28:28 +0000

    added trending and human friendly datestamps as well as fixed most of the styling

--- routes/clucksRouter.js
+++ routes/clucksRouter.js
@@ -5,7 +5,39 @@ const router = express.Router();
 router.get('/', (request, response) => {
     knex('clucks').orderBy('created_at', 'desc')
     .then(clucks => {
-        response.render('clucks', {clucks: clucks});
+        let hashtags = {};
+        let indexes = [];
+        clucks.forEach(element => {
+            if (element.content.includes('#')) {
+                element.content = element.content.replaceAll('\r\n', ' ');
+                element.content += ' ';
+                for (let i = 0; i < element.content.length; i++) {
+                    if (element.content[i] == '#') {
+                        indexes.push(i);
+                    }
+                }
+                if (indexes.length > 0) {
+                    for (let i = 0; i < indexes.length; i++) {
+                        for (let j = indexes[i]; j < element.content.length; j++) {
+                            if (element.content[j] == ' ') {
+                                hashtags[element.content.slice(indexes[i], j)] = hashtags[element.content.slice(indexes[i], j)] ? hashtags[element.content.slice(indexes[i], j)] += 1 : 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            indexes = [];
+            return hashtags;
+        });
+        let sorted = [];
+        for (const hash in hashtags) {
+            sorted.push([hash, hashtags[hash]]);
+        }
+        sorted.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+        response.render('clucks', {clucks: clucks, hashtags: sorted});
     });
 });
 
